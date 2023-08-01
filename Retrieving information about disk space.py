794f8ad6775bf87e352c4c04d360c1931a99bb3e
Ammar Rusydi <67135626+neptunazz@users.noreply.github.com>
2023-08-01 19:32:16 +0700

    Add files via upload

---
+++ Retrieving information about disk space.py
@@ -0,0 +1,11 @@
+import subprocess
+
+subprocess.run(["ls","-l","README.md"])
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
